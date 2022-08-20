4d495d619b49aa637fdf8f02589f1d3862204291
SpLiYvIiI <32437231+SpLiYvIiI@users.noreply.github.com>
2022-08-20 15:13:42 +0400

    some message

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        if(multiplyByAndToInclusive == 0)
+            return;
+        else {
+            int i = 0;
+            while (i <= 5){
+                System.out.println(multiplyByAndToInclusive * i++);
+            }
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -1,6 +1,21 @@
 package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
+    public boolean isPrime(int num){
+        int i = 2;
+        while(i <= Math.sqrt(num)){
+            if(num % i == 0)
+                return false;
+            i++;
+        }
+        return true;
+    }
     public void printPrimeNumbers(int printToInclusive) {
+        int i = 2;
+        while (i <= printToInclusive){
+            if(isPrime(i))
+                System.out.println(i);
+            i++;
+        }
     }
 }
